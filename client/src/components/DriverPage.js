b76c19a9360c66fd0771b1be8c58ee39c557f6e1
Twinkle <twinklekamra26@gmail.com>
2023-03-03 15:29:41 +0530

    ok

--- client/src/components/DriverPage.js
+++ client/src/components/DriverPage.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import '../style/driver.css'
+import Navbar from './Navbar'
 
 const DriverPage = () => {
     return (
         <>  
+        <Navbar/>
            <div className='text-center my-4 '>
             <span>Hii Durgesh I am Driver</span>
            </div>
