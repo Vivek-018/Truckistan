b0b5ec5e2e0bf204f872e2825d87fbf0727e87b4
durgesh <durgeshchaudhary020401@gmail.com>
2023-04-07 12:40:29 +0530

    done

--- client/src/components/Help.js
+++ client/src/components/Help.js
@@ -4,6 +4,8 @@ import Footer from "./Footer"
 import "../style/help.css";
 import { useRef } from 'react';
 import emailjs from 'emailjs-com'
+import { BsInstagram } from 'react-icons/bs'
+import { MdOutlineEmail } from 'react-icons/md'
 
 const Help = () => {
   const form = useRef();
@@ -17,6 +19,23 @@ const Help = () => {
       <Navbar />
       <section id="contact">
         <div className='container contact_conatiner'>
+
+          <div className='contact_options'>
+            <article className='contact_option'>
+              <MdOutlineEmail className='contact_option-icon' />
+              <h4>Email</h4>
+              <h6>dc035534@gmail.com</h6>
+              <a href="mailto:dc035534@gmail.com" target="_blank">Send a message</a>
+            </article>
+
+            <article className='contact_option'>
+              <BsInstagram className='contact_option-icon' />
+              <h4>Instagram</h4>
+              <h5>Any Quary</h5>
+              <a href='https://www.instagram.com/_durgesh.chaudhary/' target="_blank">Send a message</a>
+            </article>
+          </div>
+
           <form ref={form} onSubmit={sendEmail}>
             <input type="text" name='name' placeholder='Your Full Name' required />
             <input type="email" name='email' placeholder='Your Email' required />
