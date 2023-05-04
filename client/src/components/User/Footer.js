b7830c0a7a27a36f87f6d8ee5337144633dc32a2
Twinkle <twinklekamra26@gmail.com>
2023-05-04 19:17:56 +0530

    footer

--- client/src/components/User/Footer.js
+++ client/src/components/User/Footer.js
@@ -3,6 +3,7 @@ import { BsLinkedin } from 'react-icons/bs'
 import { FiInstagram } from 'react-icons/fi'
 import { GrYoutube } from 'react-icons/gr'
 import { FaTwitter } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const BottomNav = () => {
   return (
@@ -11,9 +12,10 @@ const BottomNav = () => {
             <h2>Loadkro</h2>
             <ul className='nav_item-list'>
                <li><a href='#'>Home</a></li>
-               <li><a href='#about'>About</a></li>
-               <li><a href='#experiance'>Experience</a></li>
-               <li><a href='#contact'>Contact</a></li>
+               {/* <li><a href="https://okcredit.in/blog/how-does-transportation-industry-work/">About</a></li> */}
+               <li><Link to="/book">About</Link></li>
+               <li><a href="https://www.instagram.com/_durgesh.chaudhary/">Experience</a></li>
+               <li><Link to="/help">Contact</Link></li>
             </ul>
 
             <div className='footer_socials_MainPage'>
